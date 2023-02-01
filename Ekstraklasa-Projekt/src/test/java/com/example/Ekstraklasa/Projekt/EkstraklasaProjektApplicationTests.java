a8307b0a0f292e57f9303cda49fad31fae8d8491
MFitas <mateuszfitas1990@gmail.com>
2023-02-02 00:02:06 +0100

    init

---
+++ Ekstraklasa-Projekt/src/test/java/com/example/Ekstraklasa/Projekt/EkstraklasaProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ekstraklasa.Projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkstraklasaProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
